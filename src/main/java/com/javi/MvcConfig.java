d5ddb2485a0c095638b3f6677eced1482fefa472
Javier Martinez Verneri <jamartinezverneri@gmail.com>
2022-08-14 19:39:34 -0300

    Initialazing JBC

---
+++ src/main/java/com/javi/MvcConfig.java
@@ -0,0 +1,12 @@
+package com.javi;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.javi")
+public class MvcConfig extends WebMvcConfigurerAdapter {
+}
---
+++ target/SpringMvcJBC/WEB-INF/result.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Javi
+  Date: 8/14/2022
+  Time: 7:13 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    Result is : ${result}
+</body>
+</html>
